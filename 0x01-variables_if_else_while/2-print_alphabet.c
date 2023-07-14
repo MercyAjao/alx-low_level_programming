694dc4ffe02834f0c3f0cc7ee6154821efd34f52
MercyAjao <mercyajao20@gmail.com>
2023-07-14 09:35:58 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		putchar(chr);
+	putchar('\n');
+	return (0);
+}
